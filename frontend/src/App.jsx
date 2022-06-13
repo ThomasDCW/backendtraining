d5acc836d99351dcb9695a1a7fca7e8d3cce80c4
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-13 14:35:01 +0200

    add pianos pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "@components/Header";
 import NavBar from "@components/NavBar";
 import Guitares from "@pages/Guitares";
+import Pianos from "@pages/Pianos";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/guitares" element={<Guitares />} />
+        <Route path="/pianos" element={<Pianos />} />
       </Routes>
     </div>
   );
