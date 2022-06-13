0db00cbf2f86d8875030b6191aa63b7d68f60ea8
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-13 11:43:39 +0200

    add guitares route

--- backend/src/router.js
+++ backend/src/router.js
@@ -10,4 +10,6 @@ router.put("/items/:id", ItemController.edit);
 router.post("/items", ItemController.add);
 router.delete("/items/:id", ItemController.delete);
 
+router.get("/guitares", ItemController.browse);
+
 module.exports = router;
