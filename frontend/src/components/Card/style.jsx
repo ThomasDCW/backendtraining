d8038c0521d92cccd8e72dd45d7f44a094317fa3
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-13 09:34:05 +0200

    add style on card

--- frontend/src/components/Card/style.jsx
+++ frontend/src/components/Card/style.jsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
 export default styled.article`
+  margin-top: 4vh;
   background-color: gold;
   border: solid 2px black;
+  width: 200px;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
