8c38d5696ad06245e9c1413c59de0772eba42c17
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-14 11:03:02 +0200

    start admin form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "@components/Header";
 import NavBar from "@components/NavBar";
+import AdminGuitares from "@pages/AdminGuitares";
 import Guitares from "@pages/Guitares";
 import Pianos from "@pages/Pianos";
 import { Route, Routes } from "react-router-dom";
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/guitares" element={<Guitares />} />
+        <Route path="/adminguitares" element={<AdminGuitares />} />
         <Route path="/pianos" element={<Pianos />} />
       </Routes>
     </div>
---
+++ frontend/src/pages/AdminGuitares.jsx
@@ -0,0 +1,12 @@
+import SAdmin from "./style";
+
+export default function AdminGuitares() {
+  return (
+    <SAdmin>
+      <input type="text" placeholder="Marque de la guitare" />
+      <input type="text" placeholder="Model de la guitare" />
+      <input type="text" placeholder="image de la guitare" />
+      <input type="text" placeholder="Prix de la guitare" />
+    </SAdmin>
+  );
+}
